75296fdaccfec2047a7dfc775f93607e88098c2d
Your Name <darrehanrasool@gmail.com>
2025-05-01 20:43:53 +0530

    Dar Rehan Rasool
    sycomusiclover

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,15 +1,38 @@
-import { AiOutlineInstagram, AiOutlineTwitter, AiFillGithub, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
-import { SiPeerlist } from 'react-icons/si';
-import { FaBlog } from 'react-icons/fa';
+import {
+  AiOutlineInstagram,
+  AiOutlineTwitter,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import { SiPeerlist } from "react-icons/si";
+import { FaBlog, FaProductHunt } from "react-icons/fa";
 
 const data = [
-    { id: 1, link: 'https://github.com/Darrehan', icon: <AiFillGithub /> },
-    { id: 2, link: 'https://www.linkedin.com/in/darrehanrasool/', icon: <AiFillLinkedin /> },
-    { id: 3, link: 'https://www.youtube.com/@RehansQuest', icon: <AiFillYoutube /> },
-    { id: 4, link: 'https://x.com/CseRehan', icon: <AiOutlineTwitter /> },
-    { id: 5, link: 'https://www.instagram.com/dar.rehan_/', icon: <AiOutlineInstagram /> },
-    { id: 6, link: 'https://peerlist.io/darrehan', icon: <SiPeerlist /> },
-    { id: 7, link: 'https://yourblog.com', icon: <FaBlog /> }  // Replace with actual blog link
+  { id: 1, link: "https://github.com/Darrehan", icon: <AiFillGithub /> },
+  {
+    id: 2,
+    link: "https://www.linkedin.com/in/darrehanrasool/",
+    icon: <AiFillLinkedin />,
+  },
+  {
+    id: 3,
+    link: "https://www.youtube.com/@RehansQuest",
+    icon: <AiFillYoutube />,
+  },
+  { id: 4, link: "https://x.com/CseRehan", icon: <AiOutlineTwitter /> },
+  {
+    id: 5,
+    link: "https://www.instagram.com/dar.rehan_/",
+    icon: <AiOutlineInstagram />,
+  },
+  { id: 6, link: "https://peerlist.io/darrehan", icon: <SiPeerlist /> },
+  { id: 7, link: "https://yourblog.com", icon: <FaBlog /> }, // Replace with actual blog link
+  {
+    id: 8,
+    link: "https://www.producthunt.com/@darrehan",
+    icon: <FaProductHunt />,
+  },
 ];
 
 export default data;
