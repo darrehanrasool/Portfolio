085361587015edcda34aa2188dea0e8f53ef6bcc
Your Name <darrehanrasool@gmail.com>
2025-07-15 16:39:35 +0530

    Dar Rehan Rasool
    sycomusiclover

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,4 +1,4 @@
-import AboutImage from "../../assets/Rehan/About3.png";
+import AboutImage from "../../assets/Rehan/About4.png";
 import CV from "../../assets/certifications/resume.pdf";
 import { HiDownload } from "react-icons/hi";
 import { TbUserHeart } from "react-icons/tb";
