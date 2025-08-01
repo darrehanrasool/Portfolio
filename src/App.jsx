b9d4d4d123f2996734588b01d1ffab4bfb24bd53
Your Name <darrehanrasool@gmail.com>
2025-08-01 15:28:28 +0530

    Dar Rehan Rasool
    sycomusiclover

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import Theme from "./theme/Theme";
 import { useThemeContext } from "./context/theme-context";
 import { useRef, useState, useEffect } from "react";
 import Certification from "./sections/certification/Certification";
-
 const App = () => {
   const { themeState } = useThemeContext();
 
