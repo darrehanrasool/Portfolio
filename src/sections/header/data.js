975dc5a606d0548a1d8bccd131d02a4c8551a539
Dar Rehan Rasool <darrehanrasool@gmail.com>
2026-03-27 13:52:10 +0530

    دار ریحان رسول
     دار ریحان رسول

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -8,7 +8,6 @@ import {
 } from "react-icons/ai";
 import { SiPeerlist } from "react-icons/si";
 import { FaThreads, FaCloud, FaBlog } from "react-icons/fa6";
-import { DiGitBranch } from "react-icons/di";
 import { IoShareSocialOutline } from "react-icons/io5";
 
 const data = [
@@ -32,55 +31,49 @@ const data = [
   },
   {
     id: 4,
-    link: "https://orcid.org/0009-0004-6426-5195",
-    icon: <DiGitBranch />,
-    tooltip: "ORCID",
-  },
-  {
-    id: 5,
     link: "https://www.youtube.com/@darrehanrasool",
     icon: <AiFillYoutube />,
     tooltip: "YouTube",
   },
 
   {
-    id: 6,
+    id: 5,
     link: "https://www.facebook.com/dar.rehan.rasool/",
     icon: <AiFillFacebook />,
     tooltip: "Facebook",
   },
   {
-    id: 7,
+    id: 6,
     link: "https://www.instagram.com/darrehanrasool/",
     icon: <AiOutlineInstagram />,
     tooltip: "Instagram",
   },
   {
-    id: 8,
+    id: 7,
     link: "https://bsky.app/profile/darrehanrasool.bsky.social",
     icon: <FaCloud />,
     tooltip: "Bluesky",
   },
   {
-    id: 9,
+    id: 8,
     link: "https://mastodon.social/@darrehanrasool",
     icon: <IoShareSocialOutline />,
     tooltip: "Mastodon",
   },
   {
-    id: 10,
+    id: 9,
     link: "https://x.com/darrehanrasool",
     icon: <AiOutlineTwitter />,
     tooltip: "Twitter",
   },
   {
-    id: 11,
+    id: 10,
     link: "https://www.threads.com/@darrehanrasool",
     icon: <FaThreads />,
     tooltip: "Threads",
   },
   {
-    id: 12,
+    id: 11,
     link: "https://yourblog.com",
     icon: <FaBlog />,
     tooltip: "Blog",
